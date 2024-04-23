db2e4c382c88a56576d6e45529b650561a53dedc
barmc <151999059+barmcoovy@users.noreply.github.com>
2024-04-23 11:28:46 +0200

    Add files via upload

---
+++ Models/PrzedmiotModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvmm_zadanie_1.Models
+{
+    public class PrzedmiotModel
+    {
+        public string Nazwa {  get; set; }
+    }
+}
